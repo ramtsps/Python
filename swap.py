2236eb6c20cab4a46209bd3de278d6ea5a506a21
ramtsps <116824915+ramtsps@users.noreply.github.com>
2023-10-18 14:36:34 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,12 @@
+def exchange(x,y):
+    x,y=y,x
+    print("After exchange of x,y")
+    print("x=",x)
+    print("y=",y)
+x=input("enter value of x")
+y=input("enter value of y")
+print("Before exchange of x,y")
+print("x=",x)
+print("y=",y)
+exchange(x,y)
+        
